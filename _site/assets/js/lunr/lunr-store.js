0a86f0329be187b82d8fb8573a9bb88f57ce5b55
sora81dev <sora81dev@gmail.com>
2025-09-16 21:20:49 +0900

    Add: Reaction Button

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,19 +5,19 @@ var store = [{
         "url": "/1/",
         "teaser": null
       },{
-        "title": "GitHub Pagesで、「Error: The process '/opt/hostedtoolcache/Ruby/3.1.6/x64/bin/gem' failed with exit code 1」と出た時の対処法",
+        "title": "GitHub Pagesで「Error: The process '/opt/hostedtoolcache/Ruby/3.1.6/x64/bin/gem' failed with exit code 1」と出た時の対処法",
         "excerpt":"解決方法  Rubyの要求バージョンを上げる      rubyのバージョンを確認する              github/workflowsの中のjekyll.ymlを開く。            40行目あたりを、先ほど確認したバージョンに変える          バージョンはそのまま入れるわけではなく、a.b.c (上の図の3.4.2)のa.b(上の図の3.4)のみを入れる    jobs:     build:         ...         steps:             ...             - name: Setup Ruby                 ...                 with:                      ruby-version: 'x.x'   ","categories": ["GitHub"],
         "tags": [],
         "url": "/2/",
         "teaser": null
       },{
         "title": "gitで「error: remote origin already exists」と表示された時の対処法",
-        "excerpt":"エラーの意味  error: remote origin already existsの意味は読んで字の如く、   リモートオリジンを追加しようとしたけど、もうあるから追加できないよ   という意味です   解決方法   $ git remote rm origin   これで現在のリモートレポジトリが削除されます。  ","categories": ["git"],
+        "excerpt":"エラーの意味  error: remote origin already existsの意味は読んで字の如く、   リモートオリジンを追加しようとしたけど、もうあるから追加できないよ   という意味です   解決方法   git remote rm origin   これで現在のリモートレポジトリが削除されます。  ","categories": ["git"],
         "tags": [],
         "url": "/3/",
         "teaser": null
       },{
-        "title": "GitHub Pagesで、「Error: The process '/opt/hostedtoolcache/Ruby/3.4.1/x64/bin/bundle' failed with exit code 16」と出た時の対処法",
+        "title": "GitHub Pagesで「Error: The process '/opt/hostedtoolcache/Ruby/3.4.1/x64/bin/bundle' failed with exit code 16」と出た時の対処法",
         "excerpt":"エラーの原因  Gemfile.lock に、GitHub Pagesで使われるLinux(x86_64)向けの情報がないため。   解決方法  Linux用のプラットフォーム情報をGemfile.lockに追加する   bundle lock --add-platform x86_64-linux   bundle lock --add-platform ruby  ","categories": ["GitHub"],
         "tags": [],
         "url": "/4/",
